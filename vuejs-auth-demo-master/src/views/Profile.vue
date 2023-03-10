2c09ec202901a51d0d62503eab6d0fccbc83a3c7
alily23 <77622822+alily23@users.noreply.github.com>
2023-03-10 23:25:43 +0100

    v9 + test image 8

--- vuejs-auth-demo-master/src/views/Profile.vue
+++ vuejs-auth-demo-master/src/views/Profile.vue
@@ -11,7 +11,7 @@
     <img src='../assets/tunetueraspoint.png' alt="Avatar" style="width:100px;height:150px;">
     <img src='../assets/vpourvendetta.png' alt="Avatar" style="width:100px;height:150px;">
 
-    <div v-for="film in films" :key="film._id">
+    <div v-for="film in localStorage.films" :key="film._id">
       <div class="img-container">
         <img v-bind:src="film.image" alt="">
       </div>
