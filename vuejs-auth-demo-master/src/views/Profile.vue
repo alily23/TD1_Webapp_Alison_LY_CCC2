11d1ee3314cc89e7f62585956ef3eda44a47ce05
alily23 <77622822+alily23@users.noreply.github.com>
2023-03-10 22:04:03 +0100

    v9 + test image 7

--- vuejs-auth-demo-master/src/views/Profile.vue
+++ vuejs-auth-demo-master/src/views/Profile.vue
@@ -6,7 +6,11 @@
     <h2 class="card__title2"> Films </h2>
     <br>
 
-    <img src='../assets/avatar.png' alt="Avatar">
+    <img src='../assets/avatar.png' alt="Avatar" style="width:500px;height:700px;">
+    <img src='../assets/avengers.png' alt="Avatar" style="width:500px;height:700px;">
+    <img src='../assets/bladerunner.png' alt="Avatar" style="width:500px;height:700px;">
+    <img src='../assets/tunetueraspoint.png' alt="Avatar" style="width:500px;height:700px;">
+    <img src='../assets/vpourvendetta.png' alt="Avatar" style="width:500px;height:700px;">
 
     <div class="form-row">
       <button @click="logout()" class="button">
